960b576f1874aac6662333ae3eb212494b432135
Pwalhout03 <91482789+Pwalhout03@users.noreply.github.com>
2023-09-27 22:43:02 +0200

    info card closes onclick

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll('.school, .travel, .foodAndDrinks, .stores, .tourism, .activities');
 const card = document.getElementById("infoCard")
+const closeButton = document.getElementById("closeButton")
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
@@ -12,3 +13,7 @@ function yourFunction(buttonId) {
     card.style.display = "block";
     console.log('Button clicked with id:', buttonId);
 }
+
+closeButton.addEventListener('click', function() {
+    card.style.display = "none";
+})
