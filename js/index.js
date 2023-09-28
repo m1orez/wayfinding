f5a9d4cf72889c199ac30890c151093e6da90ac8
Pwalhout03 <91482789+Pwalhout03@users.noreply.github.com>
2023-09-28 22:32:24 +0200

    images

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll('.school, .travel, .foodAndDrinks, .stores, .tourism, .activities');
 const card = document.getElementById("infoCard")
+const cardImg = document.getElementById("locatieImg")
 const closeButton = document.getElementById("closeButton")
 
 buttons.forEach(button => {
@@ -10,7 +11,9 @@ buttons.forEach(button => {
 });
 
 function yourFunction(buttonId) {
+    const imgLink = './img/' + buttonId + '.jpeg';
     card.style.display = "block";
+    cardImg.src = imgLink;
     console.log('Button clicked with id:', buttonId);
 }
 
